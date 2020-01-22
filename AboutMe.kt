bfb52e81aacd9e0421ae310461efad6f42496e8e
Ammy28 <47332786+Ammy28@users.noreply.github.com>
2020-01-22 09:09:40 -0800

    Add files via upload

---
+++ AboutMe.kt
@@ -0,0 +1,8 @@
+fun main() {
+    println("I love video games, anime and reading.")
+    println("I play D&D as up to 5 times a week.")
+    println("I have a dog named Sierra.")
+    println("I love talking to friends.")
+
+
+}
