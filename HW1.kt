dbb5ceac190d77d63461136d295cf3607a921a96
Ammy28 <47332786+Ammy28@users.noreply.github.com>
2020-01-27 09:11:17 -0800

    Add files via upload

---
+++ HW1.kt
@@ -0,0 +1,11 @@
+fun main(){
+    var yellow: Byte = 1
+    var orange:Short = 3
+    var purple:Int = 99
+    var white:Long = 100009
+    var black:Float = 3.14F
+    var blue:Double = 78.8889
+    var grey:Boolean = true
+    var green:Char = 'o'
+
+}
