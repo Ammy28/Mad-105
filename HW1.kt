db3a3d817d1223aab6fdcc2225ab1601024a6c27
Ammy28 <47332786+Ammy28@users.noreply.github.com>
2020-01-27 09:21:22 -0800

    Update HW1.kt

--- HW1.kt
+++ HW1.kt
@@ -1,11 +1,28 @@
 fun main(){
     var yellow: Byte = 1
+    println(yellow)
+    println(" byte 8-bit signed integer. Can be used in place of int to save memory.")
     var orange:Short = 3
+    println(orange)
+    println("short - 16-bit signed integer. Can be used in place of int to save memory.")
     var purple:Int = 99
+    println(purple)
+    println("int - 32 bit signed integer.")
     var white:Long = 100009
+    println(white)
+    println("long - 64 bit integer")
     var black:Float = 3.14F
+    println(black)
+    println("float - Single-precision floating point (small number with a decimal point)")
     var blue:Double = 78.8889
+    println(blue)
+    println("double - 64 bit floating point (large number with a decimal point)")
     var grey:Boolean = true
+    println(grey)
+    println("boolean - true / false values")
     var green:Char = 'o'
+    println(green)
+    println("char - single 16-bit Unicode Character")
+
 
 }
