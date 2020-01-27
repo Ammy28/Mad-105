6c6d9007918de692b3b9bac097211f37d89f954b
Ammy28 <47332786+Ammy28@users.noreply.github.com>
2020-01-27 09:34:13 -0800

    Add files via upload

---
+++ HW2.kt
@@ -0,0 +1,17 @@
+fun main(){
+    var x = 5
+    var y = 2
+    var z = 6
+    var a = 21
+    println("5 + 5 = 10")
+    println(x + x == 10)
+    println("5 - 2 = 3")
+    println(x - y == 3)
+    println("2 * 6 = 12")
+    println(y * z == 12)
+    println("6/2 == 3")
+    println(z/y == 3)
+    println("21%5 = 1")
+    println(a%x == 1)
+
+}
