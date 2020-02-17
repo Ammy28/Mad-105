c1a3a7f5f3bfa77bee8f2c5eafa7cdb85f452744
Ammy28 <47332786+Ammy28@users.noreply.github.com>
2020-02-17 11:32:51 -0600

    Add files via upload

---
+++ Employee.kt
@@ -0,0 +1,8 @@
+class Employee(var empNum: Int, var empName: String, var empShift: Int) {
+
+    fun getInfo(){
+            println("Employee Name: $empName\nEmployee Number: $empNum\nEmployee Shift: $empShift")
+        }
+
+    }
+
---
+++ HW13.kt
@@ -0,0 +1,9 @@
+fun main() {
+var emp1 = Employee(1,"Jonathan Young", 1)
+   emp1.getInfo()
+    var emp2 = Employee(2,"Jason Paige", 2)
+    emp2.getInfo()
+    var emp3 = Employee(3,"Caleb Hyles", 3)
+    emp3.getInfo()
+
+}
